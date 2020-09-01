103ab6ee115148cd79a987d09802b4ec0e9976d8
Martins Silins <kydzoster@gmail.com>
2020-09-01 18:02:21 +0000

    added pagination to tables

--- Happy_Feets/settings.py
+++ Happy_Feets/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = '/static/'
 # These are a locations for images
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- animals/views.py
+++ animals/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from .models import Animal
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def home(request):
+
+    animals = Animal.objects.all()
+    mammals = Animal.objects.filter(Q(animal='Cat') | Q(animal='Dog'))
+    paginator1 = Paginator(mammals, 1)
+    page = request.GET.get('mammalspage')
+    mammals = paginator1.get_page(page)
+
+    others = Animal.objects.filter(Q(animal='Reptile') | Q(animal='Bird'))
+    paginator2 = Paginator(others, 1)
+    page = request.GET.get('otherspage')
+    others = paginator2.get_page(page)
+
     context = {
-        'animals': Animal.objects.all(),
+        'animals': animals,
+        'mammals': mammals,
+        'others': others,
     }
     return render(request, 'home.html', context)
 
