2cab8f2afef01f081401e95863651e9d8f75a48f
Martins Silins <kydzoster@gmail.com>
2020-09-02 17:20:05 +0000

    added details page for each animal

--- animals/views.py
+++ animals/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Animal
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 
 
 def home(request):
@@ -23,6 +24,18 @@ def home(request):
         'others': others,
     }
     return render(request, 'home.html', context)
+
+
+def detail(request, animal_id):
+    # find an animal with this id if it does not exist pass 404
+    animal = get_object_or_404(Animal, pk=animal_id)
+    animals = Animal.objects.all()
+
+    context = {
+        'animal': animal,
+        'animals': animals,
+    }
+    return render(request, 'detail.html', context)
 
 
 def search(request):
